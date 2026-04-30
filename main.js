4f297920625bc063d1bd983e01210a55dcbbdf04
akisuteno-game <akisuteno5160@gmail.com>
2026-04-30 20:25:11 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
+const list = document.getElementById("list");
 
+enemies.forEach(e=>{
+  const btn = document.createElement("button");
+  btn.textContent = e.name;
+
+  btn.onclick = ()=>{
+    location.href = "battle.html?enemy="+e.id;
+  };
+
+  list.appendChild(btn);
+  list.appendChild(document.createElement("br"));
+});
