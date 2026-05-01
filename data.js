5bff4bcd810236ef6e8a7be226c78162a6c0336b
akisuteno-game <akisuteno5160@gmail.com>
2026-05-02 08:19:59 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,8 +1,43 @@
 const enemies = [
-  {id:0,name:"スライム",hp:10,atk:2,speed:1,drop:"ゼリー"},
-  {id:1,name:"ゴブリン",hp:15,atk:3,speed:1.2,drop:"牙"},
-  {id:2,name:"オーク",hp:25,atk:4,speed:0.9,drop:"骨"},
-  {id:3,name:"ドラゴン",hp:40,atk:6,speed:0.7,drop:"鱗"}
+  {
+    id:0,
+    name:"スライム",
+    icon:"🟢",
+    hp:10,
+    atk:2,
+    speed:1,
+    drop:"ゼリー"
+  },
+
+  {
+    id:1,
+    name:"ゴブリン",
+    icon:"👺",
+    hp:15,
+    atk:3,
+    speed:1.2,
+    drop:"牙"
+  },
+
+  {
+    id:2,
+    name:"オーク",
+    icon:"🐗",
+    hp:25,
+    atk:4,
+    speed:0.9,
+    drop:"骨"
+  },
+
+  {
+    id:3,
+    name:"ドラゴン",
+    icon:"🐲",
+    hp:40,
+    atk:6,
+    speed:0.7,
+    drop:"鱗"
+  }
 ];
 
 const itemIcons = {
