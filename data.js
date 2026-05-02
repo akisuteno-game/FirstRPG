1ff7c4d33515570d5c14914bdd6a70f43ed99e89
akisuteno-game <akisuteno5160@gmail.com>
2026-05-02 21:58:43 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,62 @@
-// data loaded
+const enemies = [
+
+  {
+    id:0,
+
+    name:"スライム",
+
+    img:"img/monsters/slime.png",
+
+    hp:10,
+
+    atk:2,
+
+    speed:1
+  },
+
+
+  {
+    id:1,
+
+    name:"ゴブリン",
+
+    img:"img/monsters/goblin.png",
+
+    hp:20,
+
+    atk:3,
+
+    speed:1
+  },
+
+
+  {
+    id:2,
+
+    name:"オーク",
+
+    img:"img/monsters/orc.png",
+
+    hp:30,
+
+    atk:4,
+
+    speed:1
+  },
+
+
+  {
+    id:3,
+
+    name:"ドラゴン",
+
+    img:"img/monsters/dragon.png.PNG",
+
+    hp:50,
+
+    atk:6,
+
+    speed:1
+  }
+
+];
