594f213ec5d50b6a7cb12a9847c15469724dc7d8
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 08:21:26 +0900

    Create debug.js

---
+++ img/monsters/debug.js
@@ -0,0 +1,26 @@
+document.addEventListener(
+  "DOMContentLoaded",
+
+  function(){
+
+
+    alert(
+
+      "player : " +
+      typeof renderPlayer +
+
+      "\nenemy : " +
+      typeof renderEnemyTab +
+
+      "\nupgrade : " +
+      typeof renderUpgradeTab +
+
+      "\nsetting : " +
+      typeof renderSettingTab
+
+    );
+
+
+  }
+
+);
