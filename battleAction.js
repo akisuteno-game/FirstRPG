f899004f5cd7b1433ce11fafa1bf3c42dc3776f3
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 12:37:11 +0900

    Update battleAction.js

--- battleAction.js
+++ battleAction.js
@@ -1,9 +1,155 @@
 function attackEnemy(){
 
 
-  alert(
-    "攻撃成功"
-  );
+  if(
+    !currentEnemy
+  ){
+
+    return;
+
+  }
+
+
+
+
+  currentEnemy.hp -=
+    player.atk;
+
+
+  if(
+    currentEnemy.hp < 0
+  ){
+
+    currentEnemy.hp = 0;
+
+  }
+
+
+
+
+  const hpText =
+
+    document.getElementById(
+      "enemyHpText"
+    );
+
+
+  if(
+    hpText
+  ){
+
+    hpText.innerHTML =
+      currentEnemy.hp;
+
+  }
+
+
+
+
+  const hpBar =
+
+    document.getElementById(
+      "enemyHpFill"
+    );
+
+
+  if(
+    hpBar
+  ){
+
+      const maxHp =
+
+        enemies[
+          currentEnemy.id
+        ].hp;
+
+
+      const percent =
+
+        (
+          currentEnemy.hp
+          /
+          maxHp
+        ) * 100;
+
+
+      hpBar.style.width =
+
+        percent + "%";
+
+  }
+
+
+
+
+  if(
+    currentEnemy.hp <= 0
+  ){
+
+    alert(
+      "勝利！"
+    );
+
+
+    localStorage.removeItem(
+      "selectedEnemy"
+    );
+
+
+    location.href =
+      "index.html";
+
+
+    return;
+
+  }
+
+
+
+
+  enemyAttack();
+
+
+  startPlayerGauge();
+
+
+}
+
+
+
+
+function enemyAttack(){
+
+
+  player.hp -=
+    currentEnemy.atk;
+
+
+  if(
+    player.hp < 0
+  ){
+
+    player.hp = 0;
+
+  }
+
+
+  renderPlayer();
+
+
+  if(
+    player.hp <= 0
+  ){
+
+    alert(
+      "やられた..."
+    );
+
+
+    location.href =
+      "index.html";
+
+  }
 
 
 }
