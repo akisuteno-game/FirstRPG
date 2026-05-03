9a621cb8d42271945f73f72060108151c5b82c17
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 21:55:26 +0900

    Update debug.js

--- debug.js
+++ debug.js
@@ -1,1 +1,34 @@
+console.log("debug ok");
 
+
+
+
+document.body.insertAdjacentHTML(
+
+  "beforeend",
+
+  `
+
+    <div
+
+      style="
+
+        position:fixed;
+        top:10px;
+        right:10px;
+        background:black;
+        color:lime;
+        padding:10px;
+        z-index:99999;
+
+      "
+
+    >
+
+      DEBUG OK
+
+    </div>
+
+  `
+
+);
