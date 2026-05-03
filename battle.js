1613eb0b33d3afaaf954ff5cb23c68aea6a6dde9
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 11:22:33 +0900

    Update battle.js

--- battle.js
+++ battle.js
@@ -1,218 +1,89 @@
-let currentEnemy = null;
+const player = {
 
+  hp:30,
 
+  maxHp:30,
 
+  atk:5,
 
-window.onload =
-function(){
-
-
-  renderPlayer();
-
-
-  loadEnemy();
-
+  crit:5
 
 };
 
 
 
 
-function loadEnemy(){
+function renderPlayer(){
 
 
-  const area =
+  const ui =
     document.getElementById(
-      "battleArea"
+      "playerUI"
     );
 
 
-  if(!area){
+  if(!ui){
 
     return;
 
   }
-
-
-  const saved =
-
-    localStorage.getItem(
-      "selectedEnemy"
-    );
-
-
-  if(!saved){
-
-    area.innerHTML =
-
-      "敵が選ばれていません";
-
-    return;
-
-  }
-
-
-  currentEnemy =
-
-    JSON.parse(
-      saved
-    );
-
-
-  area.innerHTML = `
-
-    <div
-      style="
-        text-align:center;
-        color:white;
-        padding:30px;
-      "
-    >
-
-      <h1>
-
-        ${currentEnemy.name}
-
-      </h1>
-
-
-      <img
-
-        src="${currentEnemy.img}"
-
-        width="220"
-
-      >
-
-
-      <br><br>
-
-
-      HP :
-
-      <span
-        id="enemyHpText"
-      >
-
-        ${currentEnemy.hp}
-
-      </span>
-
-
-      <div class="bar">
-
-        <div
-
-          id="enemyHpFill"
-
-          class="fill"
-
-          style="
-            width:100%;
-          "
-
-        >
-        </div>
-
-      </div>
-
-
-      <br>
-
-
-      <button
-        onclick="
-          attackEnemy()
-        "
-      >
-
-        攻撃
-
-      </button>
-
-
-    </div>
-
-  `;
-
-
-}
-
-
-
-
-function attackEnemy(){
-
-
-  currentEnemy.hp -=
-    player.atk;
-
-
-  if(
-    currentEnemy.hp < 0
-  ){
-
-    currentEnemy.hp = 0;
-
-  }
-
-
-  document
-    .getElementById(
-      "enemyHpText"
-    )
-    .innerHTML =
-
-      currentEnemy.hp;
-
-
-  const maxHp =
-    enemies[
-      currentEnemy.id
-    ].hp;
 
 
   const percent =
 
     (
-      currentEnemy.hp
+      player.hp
       /
-      maxHp
+      player.maxHp
     ) * 100;
 
 
-  document
-    .getElementById(
-      "enemyHpFill"
-    )
-    .style.width =
+  ui.innerHTML = `
 
-      percent + "%";
+    HP :
 
+    ${player.hp}
 
-  if(
-    currentEnemy.hp <= 0
-  ){
+    /
 
-    localStorage.removeItem(
-      "selectedEnemy"
-    );
+    ${player.maxHp}
 
 
-    setTimeout(
-      function(){
+    <div
+      class="bar"
+    >
 
-        location.href =
-          "index.html";
+      <div
 
-      },
+        class="fill"
 
-      1000
+        style="
+          width:${percent}%;
+        "
 
-    );
+      >
+      </div>
+
+    </div>
 
 
-  }
+    <br>
+
+
+    攻撃 :
+
+    ${player.atk}
+
+
+    <br><br>
+
+
+    クリ率 :
+
+    ${player.crit}%
+
+
+  `;
 
 
 }
