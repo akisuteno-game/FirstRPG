f9501d98ea64fa9b6ebf903417acee7c460f1640
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 16:05:20 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,46 +1,100 @@
 const enemies = [
 
   {
+
     id:0,
+
     name:"スライム",
-    img:"img/monsters/slime.png",
+
     hp:10,
+
     atk:2,
-    speed:1500,
-    drop:5
+
+    speed:2500,
+
+    drop:5,
+
+    material:"スライムゼリー",
+
+    rareMaterial:"キングゼリー",
+
+    img:"img/monsters/slime.png"
+
   },
 
 
+
+
   {
+
     id:1,
+
     name:"ゴブリン",
-    img:"img/monsters/goblin.png",
+
     hp:20,
+
     atk:3,
-    speed:1800,
-    drop:10
-  },
 
-
-  {
-    id:2,
-    name:"オーク",
-    img:"img/monsters/orc.png",
-    hp:30,
-    atk:4,
     speed:2200,
-    drop:20
+
+    drop:10,
+
+    material:"ゴブリンの牙",
+
+    rareMaterial:"王族の牙",
+
+    img:"img/monsters/goblin.png"
+
   },
 
 
+
+
   {
+
+    id:2,
+
+    name:"オーク",
+
+    hp:30,
+
+    atk:4,
+
+    speed:2000,
+
+    drop:15,
+
+    material:"オークの皮",
+
+    rareMaterial:"古代の皮",
+
+    img:"img/monsters/orc.png"
+
+  },
+
+
+
+
+  {
+
     id:3,
+
     name:"ドラゴン",
-    img:"img/monsters/dragon.png",
+
     hp:50,
+
     atk:6,
-    speed:3000,
-    drop:50
+
+    speed:1500,
+
+    drop:30,
+
+    material:"竜のウロコ",
+
+    rareMaterial:"紅竜の心臓",
+
+    img:"img/monsters/dragon.png"
+
   }
 
 ];
