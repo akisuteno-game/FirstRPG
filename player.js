a41aca4d6cf9b5910b2ba14cf808dcb9c970fa4d
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 10:06:01 +0900

    Update player.js

--- player.js
+++ player.js
@@ -2,6 +2,8 @@ const player = {
 
   hp:100,
 
+  maxHp:100,
+
   atk:10,
 
   crit:5
@@ -27,22 +29,108 @@ function renderPlayer(){
   }
 
 
+  const hpPercent =
+    (
+      player.hp
+      /
+      player.maxHp
+    ) * 100;
+
+
   ui.innerHTML = `
 
     HP :
-    ${player.hp}
+    <span id="hpText">
 
-    <br><br>
+      ${player.hp}
+      /
+      ${player.maxHp}
+
+    </span>
+
+
+    <div class="bar">
+
+      <div
+        id="hpFill"
+        class="fill"
+
+        style="
+          width:${hpPercent}%;
+        "
+      >
+      </div>
+
+    </div>
+
+
+    <br>
+
 
     攻撃 :
     ${player.atk}
 
+
     <br><br>
+
 
     クリ率 :
     ${player.crit}%
 
+
   `;
+
+
+}
+
+
+
+
+function updateHP(){
+
+
+  const hpFill =
+    document.getElementById(
+      "hpFill"
+    );
+
+
+  const hpText =
+    document.getElementById(
+      "hpText"
+    );
+
+
+  if(!hpFill){
+
+    return;
+
+  }
+
+
+  const hpPercent =
+    (
+      player.hp
+      /
+      player.maxHp
+    ) * 100;
+
+
+  hpFill.style.width =
+    hpPercent + "%";
+
+
+  hpText.innerHTML =
+
+    player.hp
+
+    +
+
+    "/"
+
+    +
+
+    player.maxHp;
 
 
 }
