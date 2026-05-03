e7a7f59d2a1a7c70f5d3f025bee3437a69b61128
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 10:10:10 +0900

    Update enemyTab.js

--- enemyTab.js
+++ enemyTab.js
@@ -23,13 +23,23 @@ function renderEnemyTab(){
     list.innerHTML += `
 
       <div
+
         class="enemyCard"
+
+        onclick="
+          selectEnemy(
+            ${enemy.id}
+          )
+        "
+
       >
 
         <img
+
           class="enemyImage"
 
           src="${enemy.img}"
+
         >
 
 
@@ -59,6 +69,77 @@ function renderEnemyTab(){
 
 
   });
+
+
+}
+
+
+
+
+function selectEnemy(enemyId){
+
+
+  const enemy =
+    enemies.find(
+      e=>e.id===enemyId
+    );
+
+
+  if(!enemy){
+
+    return;
+
+  }
+
+
+  const battleArea =
+    document.getElementById(
+      "battleArea"
+    );
+
+
+  if(!battleArea){
+
+    return;
+
+  }
+
+
+  battleArea.innerHTML = `
+
+    <h2>
+
+      ${enemy.name}
+
+    </h2>
+
+
+    <img
+
+      src="${enemy.img}"
+
+      style="
+        width:200px;
+      "
+
+    >
+
+
+    <br><br>
+
+
+    HP :
+    ${enemy.hp}
+
+
+    <br>
+
+
+    ATK :
+    ${enemy.atk}
+
+
+  `;
 
 
 }
