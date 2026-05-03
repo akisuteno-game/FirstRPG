b3b3a0644e53f818adc604e623215a3b8fc11ac2
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 21:11:41 +0900

    Create materialIcon.js

---
+++ player/materialIcon.js
@@ -0,0 +1,111 @@
+function getMaterialPath(name){
+
+
+  if(
+    name ===
+    "スライムゼリー"
+  ){
+
+    return
+      "img/materials/slimeGel.png";
+
+  }
+
+
+
+
+  if(
+    name ===
+    "キングゼリー"
+  ){
+
+    return
+      "img/materials/kingGel.png";
+
+  }
+
+
+
+
+  if(
+    name ===
+    "ゴブリンの牙"
+  ){
+
+    return
+      "img/materials/goblinTooth.png";
+
+  }
+
+
+
+
+  if(
+    name ===
+    "王族の牙"
+  ){
+
+    return
+      "img/materials/kingTooth.png";
+
+  }
+
+
+
+
+  if(
+    name ===
+    "オークの皮"
+  ){
+
+    return
+      "img/materials/orcSkin.png";
+
+  }
+
+
+
+
+  if(
+    name ===
+    "古代の皮"
+  ){
+
+    return
+      "img/materials/ancientSkin.png";
+
+  }
+
+
+
+
+  if(
+    name ===
+    "竜のウロコ"
+  ){
+
+    return
+      "img/materials/dragonScale.png";
+
+  }
+
+
+
+
+  if(
+    name ===
+    "紅竜の心臓"
+  ){
+
+    return
+      "img/materials/dragonHeart.png";
+
+  }
+
+
+
+
+  return null;
+
+
+}
