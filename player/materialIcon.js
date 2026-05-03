4f36b972ad8d1e1868cf1067fcecffdcf470284f
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 21:26:42 +0900

    Update materialIcon.js

--- player/materialIcon.js
+++ player/materialIcon.js
@@ -6,26 +6,23 @@ function getMaterialPath(name){
     "スライムゼリー":
     "img/materials/slimeGel.png",
 
+
+
+
     "キングゼリー":
     "img/materials/kingGel.png",
+
+
+
 
     "ゴブリンの牙":
     "img/materials/goblinTooth.png",
 
+
+
+
     "王族の牙":
-    "img/materials/kingTooth.png",
-
-    "オークの皮":
-    "img/materials/orcSkin.png",
-
-    "古代の皮":
-    "img/materials/ancientSkin.png",
-
-    "竜のウロコ":
-    "img/materials/dragonScale.png",
-
-    "紅竜の心臓":
-    "img/materials/dragonHeart.png"
+    "img/materials/kingTooth.png"
 
   };
 
