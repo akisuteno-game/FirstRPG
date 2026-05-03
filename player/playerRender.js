3e9e3221126536c5671530756e54faa9d446bbee
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 15:57:46 +0900

    Create playerRender.js

---
+++ player/playerRender.js
@@ -0,0 +1,131 @@
+function renderPlayer(){
+
+
+  const ui =
+    document.getElementById(
+      "playerUI"
+    );
+
+
+
+
+  if(!ui){
+
+    return;
+
+  }
+
+
+
+
+  const percent =
+
+    (
+      player.hp
+      /
+      player.maxHp
+    ) * 100;
+
+
+
+
+  ui.innerHTML = `
+
+    HP :
+
+    ${player.hp}
+
+    /
+
+    ${player.maxHp}
+
+
+
+
+    <div class="bar">
+
+      <div
+
+        id="playerHpFill"
+
+        class="fill"
+
+        style="
+          width:${percent}%;
+        "
+
+      >
+      </div>
+
+    </div>
+
+
+
+
+    <br>
+
+
+
+
+    攻撃 :
+    ${player.atk}
+
+
+
+
+    <br><br>
+
+
+
+
+    クリ率 :
+    ${player.crit}%
+
+
+
+
+    <br><br>
+
+
+
+
+    GOLD :
+    ${player.gold}
+
+  `;
+
+
+
+
+  const bar =
+
+    document.getElementById(
+      "playerHpFill"
+    );
+
+
+
+
+  if(percent > 70){
+
+    bar.style.background =
+      "lime";
+
+  }
+
+  else if(percent > 30){
+
+    bar.style.background =
+      "yellow";
+
+  }
+
+  else{
+
+    bar.style.background =
+      "red";
+
+  }
+
+
+}
