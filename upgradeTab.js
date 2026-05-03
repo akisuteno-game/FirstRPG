6c9a9046325f7da3c13cd09ed0e96677d91c678c
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 10:15:38 +0900

    Update upgradeTab.js

--- upgradeTab.js
+++ upgradeTab.js
@@ -16,9 +16,59 @@ function renderUpgradeTab(){
 
   tab.innerHTML = `
 
-    強化画面
+    <div
+      style="
+        padding:20px;
+        color:white;
+      "
+    >
 
-  `;
+      <h2>
+        強化画面
+      </h2>
+
+
+      <button
+        onclick="
+          player.atk++;
+          renderPlayer();
+        "
+      >
+
+        攻撃 +1
+
+      </button>
+
+
+      <button
+        onclick="
+          player.crit++;
+          renderPlayer();
+        "
+      >
+
+        クリ率 +1%
+
+      </button>
+
+
+      <br><br>
+
+
+      現在の攻撃力 :
+      ${player.atk}
+
+
+      <br><br>
+
+
+      現在のクリ率 :
+      ${player.crit}%
+
+
+    </div>
+
+  ";
 
 
 }
