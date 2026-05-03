21db6e1b6fd01cb27baae778152845893a486e61
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 21:41:24 +0900

    Update materialTab.js

--- materialTab.js
+++ materialTab.js
@@ -20,6 +20,29 @@ function renderMaterialTab(){
 
 
 
+  const order = [
+
+    "スライムゼリー",
+
+    "キングゼリー",
+
+    "ゴブリンの牙",
+
+    "王族の牙",
+
+    "オークの皮",
+
+    "古代の皮",
+
+    "竜のウロコ",
+
+    "紅竜の心臓"
+
+  ];
+
+
+
+
   let html = `
 
     <div
@@ -28,19 +51,125 @@ function renderMaterialTab(){
       "
     >
 
-      <h2>
-
-        素材一覧
-
-      </h2>
+  `;
 
 
 
 
-      ${createMaterialHTML()}
+  order.forEach(
+
+    function(name){
+
+
+      const amount =
+
+        player.materials[
+          name
+        ];
 
 
 
+
+      if(
+        !amount
+      ){
+
+        return;
+
+      }
+
+
+
+
+      const path =
+
+        getMaterialPath(
+          name
+        );
+
+
+
+
+      let icon =
+        "？";
+
+
+
+
+      if(
+        path
+      ){
+
+        icon = `
+
+          <img
+
+            src="${path}"
+
+            onerror="
+              this.outerHTML='？'
+            "
+
+          >
+
+        `;
+
+      }
+
+
+
+
+      html += `
+
+        <div
+          class="
+            materialCard
+          "
+        >
+
+
+
+
+          ${icon}
+
+
+
+
+          <br><br>
+
+
+
+
+          ${name}
+
+
+
+
+          <br><br>
+
+
+
+
+          ×
+
+          ${amount}
+
+
+
+
+        </div>
+
+      `;
+
+
+    }
+
+  );
+
+
+
+
+  html += `
 
     </div>
 
