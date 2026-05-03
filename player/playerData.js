4b0e04ef192522858830e7c3dc0e64126e482834
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 15:56:19 +0900

    Create playerData.js

---
+++ player/playerData.js
@@ -0,0 +1,26 @@
+const defaultPlayer = {
+
+  hp:30,
+
+  maxHp:30,
+
+  atk:5,
+
+  crit:5,
+
+  gold:0
+
+};
+
+
+
+
+const player =
+
+  JSON.parse(
+
+    JSON.stringify(
+      defaultPlayer
+    )
+
+  );
