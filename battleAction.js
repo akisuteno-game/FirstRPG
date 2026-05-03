d5d88a8dbe039fdaa9e14444016f39b9b3173d8f
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 12:01:23 +0900

    Update battleAction.js

--- battleAction.js
+++ battleAction.js
@@ -23,18 +23,17 @@ function attackEnemy(){
   }
 
 
-  document
-    .getElementById(
-      "enemyHpText"
-    )
-    .innerHTML =
-
-      currentEnemy.hp;
+  updateEnemyUI();
 
 
   if(
     currentEnemy.hp <= 0
   ){
+
+    localStorage.removeItem(
+      "selectedEnemy"
+    );
+
 
     setTimeout(
 
@@ -54,7 +53,115 @@ function attackEnemy(){
   }
 
 
+  enemyAttack();
+
+
   startGauge();
+
+
+}
+
+
+
+
+function enemyAttack(){
+
+
+  player.hp -=
+    currentEnemy.atk;
+
+
+  if(
+    player.hp < 0
+  ){
+
+    player.hp = 0;
+
+  }
+
+
+  renderPlayer();
+
+
+  localStorage.setItem(
+
+    "playerData",
+
+    JSON.stringify(
+      player
+    )
+
+  );
+
+
+  if(
+    player.hp <= 0
+  ){
+
+    alert(
+      "やられた..."
+    );
+
+
+    player.hp =
+      player.maxHp;
+
+
+    setTimeout(
+
+      function(){
+
+        location.href =
+          "index.html";
+
+      },
+
+      1000
+
+    );
+
+  }
+
+
+}
+
+
+
+
+function updateEnemyUI(){
+
+
+  document
+    .getElementById(
+      "enemyHpText"
+    )
+    .innerHTML =
+
+      currentEnemy.hp;
+
+
+  const maxHp =
+    enemies[
+      currentEnemy.id
+    ].hp;
+
+
+  const percent =
+
+    (
+      currentEnemy.hp
+      /
+      maxHp
+    ) * 100;
+
+
+  document
+    .getElementById(
+      "enemyHpFill"
+    )
+    .style.width =
+
+      percent + "%";
 
 
 }
