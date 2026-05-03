b79a258d9c93bddfae6b844bda459b76b9204895
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 13:11:58 +0900

    Update battleCore.js

--- battleCore.js
+++ battleCore.js
@@ -34,7 +34,9 @@ function initBattle(){
 
 
 
-  if(!saved){
+  if(
+    !saved
+  ){
 
     return;
 
@@ -61,7 +63,9 @@ function initBattle(){
 
 
 
-  if(!area){
+  if(
+    !area
+  ){
 
     return;
 
@@ -77,12 +81,23 @@ function initBattle(){
 
 
 
-  document
-    .getElementById(
+  const button =
+
+    document.getElementById(
       "attackBtn"
-    )
-    .onclick =
+    );
+
+
+
+
+  if(
+    button
+  ){
+
+    button.onclick =
       attackEnemy;
+
+  }
 
 
 
@@ -91,6 +106,159 @@ function initBattle(){
 
 
   startEnemyGauge();
+
+
+}
+
+
+
+
+function createBattleHTML(){
+
+
+  return `
+
+    <div
+      style="
+        color:white;
+        text-align:center;
+      "
+    >
+
+      <h1>
+
+        ${currentEnemy.name}
+
+      </h1>
+
+
+
+
+      <img
+
+        src="${currentEnemy.img}"
+
+        width="220"
+
+      >
+
+      <br><br>
+
+
+
+
+      敵HP :
+
+      <span id="enemyHpText">
+
+        ${currentEnemy.hp}
+
+      </span>
+
+
+
+
+      <div class="bar">
+
+        <div
+
+          id="enemyHpFill"
+
+          class="fill"
+
+          style="
+            width:100%;
+          "
+
+        >
+        </div>
+
+      </div>
+
+
+
+
+      <br><br>
+
+
+
+
+      自分
+      2000ms
+
+
+
+
+      <div class="bar">
+
+        <div
+
+          id="playerGauge"
+
+          class="fill"
+
+          style="
+            width:0%;
+          "
+
+        >
+        </div>
+
+      </div>
+
+
+
+
+      <br><br>
+
+
+
+
+      敵
+      ${currentEnemy.speed}ms
+
+
+
+
+      <div class="bar">
+
+        <div
+
+          id="enemyGauge"
+
+          class="fill"
+
+          style="
+            width:0%;
+          "
+
+        >
+        </div>
+
+      </div>
+
+
+
+
+      <br><br>
+
+
+
+
+      <button
+        id="attackBtn"
+      >
+
+        攻撃
+
+      </button>
+
+
+
+
+    </div>
+
+  `;
 
 
 }
