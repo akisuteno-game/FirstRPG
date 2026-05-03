d08105711a34d48429a58d7c228b01e8399d5aa0
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 10:28:36 +0900

    Update settingTab.js

--- settingTab.js
+++ settingTab.js
@@ -14,42 +14,15 @@ function renderSettingTab(){
   }
 
 
-  tab.innerHTML = `
+  tab.innerHTML =
 
-    <div
-      style="
-        padding:20px;
-        color:white;
-      "
-    >
+    "<h2>設定画面</h2>" +
 
-      <h2>
+    "<button onclick='location.reload()'>" +
 
-        設定画面
+    "リロード" +
 
-      </h2>
-
-
-      <button
-        onclick="
-          location.reload()
-        "
-      >
-
-        リロード
-
-      </button>
-
-
-      <br><br>
-
-
-      Version 1.0
-
-
-    </div>
-
-  `;
+    "</button>";
 
 
 }
