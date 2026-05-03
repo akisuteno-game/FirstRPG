06001fad605abc834aecc6e42d475249e5139324
akisuteno-game <akisuteno5160@gmail.com>
2026-05-03 21:15:47 +0900

    Update materialIcon.js

--- player/materialIcon.js
+++ player/materialIcon.js
@@ -1,111 +1,38 @@
 function getMaterialPath(name){
 
 
-  if(
-    name ===
-    "スライムゼリー"
-  ){
+  const paths = {
 
-    return
-      "img/materials/slimeGel.png";
+    "スライムゼリー":
+    "img/materials/slimeGel.png",
 
-  }
+    "キングゼリー":
+    "img/materials/kingGel.png",
 
+    "ゴブリンの牙":
+    "img/materials/goblinTooth.png",
 
+    "王族の牙":
+    "img/materials/kingTooth.png",
 
+    "オークの皮":
+    "img/materials/orcSkin.png",
 
-  if(
-    name ===
-    "キングゼリー"
-  ){
+    "古代の皮":
+    "img/materials/ancientSkin.png",
 
-    return
-      "img/materials/kingGel.png";
+    "竜のウロコ":
+    "img/materials/dragonScale.png",
 
-  }
+    "紅竜の心臓":
+    "img/materials/dragonHeart.png"
 
-
-
-
-  if(
-    name ===
-    "ゴブリンの牙"
-  ){
-
-    return
-      "img/materials/goblinTooth.png";
-
-  }
+  };
 
 
 
 
-  if(
-    name ===
-    "王族の牙"
-  ){
-
-    return
-      "img/materials/kingTooth.png";
-
-  }
-
-
-
-
-  if(
-    name ===
-    "オークの皮"
-  ){
-
-    return
-      "img/materials/orcSkin.png";
-
-  }
-
-
-
-
-  if(
-    name ===
-    "古代の皮"
-  ){
-
-    return
-      "img/materials/ancientSkin.png";
-
-  }
-
-
-
-
-  if(
-    name ===
-    "竜のウロコ"
-  ){
-
-    return
-      "img/materials/dragonScale.png";
-
-  }
-
-
-
-
-  if(
-    name ===
-    "紅竜の心臓"
-  ){
-
-    return
-      "img/materials/dragonHeart.png";
-
-  }
-
-
-
-
-  return null;
+  return paths[name] || null;
 
 
 }
