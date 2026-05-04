a0d645ecda1dc5ec83dc7693d75c733c9e8b45b8
akisuteno-game <akisuteno5160@gmail.com>
2026-05-04 21:16:13 +0900

    Refactor renderBattle function for clarity

--- battle/battleRender.js
+++ battle/battleRender.js
@@ -1,132 +1,29 @@
 function renderBattle(){
 
-
   const area =
-
     document.getElementById(
       "battleArea"
     );
 
-
-
-
   if(
     !area
   ){
-
     return;
-
   }
-
-
-
-
-  if(
-    !currentEnemy
-  ){
-
-    return;
-
-  }
-
-
-
 
   area.innerHTML = `
 
-    <div class="enemyBox">
-
-
-
-
-      <img
-        src="${currentEnemy.img}"
-        class="enemyImage"
-      >
-
-
-
-
-      <div class="battleInfo">
-
-
-
-
-        <h2>
-
-          ${currentEnemy.name}
-
-        </h2>
-
-
-
-
-        <div class="hpRow">
-
-          HP :
-
-          <span id="enemyHpText">
-
-            ${currentEnemy.hp}
-
-          </span>
-
-        </div>
-
-
-
-
-        <div>
-
-          攻撃速度 :
-
-          ${currentEnemy.speed}ms
-
-          (
-
-          ${currentEnemy.speed / 1000}秒
-
-          )
-
-        </div>
-
-
-
-
-        <div class="gaugeBox">
-
-          <div
-            id="enemyHpFill"
-            class="gaugeFill"
-            style="
-              width:100%;
-              background:lime;
-            "
-          ></div>
-
-        </div>
-
-
-
-
-        ${renderGauges()}
-
-
-
-
-        ${renderButtons()}
-
-
-
-
-      </div>
-
-
-
-
-    </div>
+    <h1
+      style="
+        color:white;
+        font-size:48px;
+        text-align:center;
+        margin-top:100px;
+      "
+    >
+      BATTLE OK
+    </h1>
 
   `;
-
 
 }
