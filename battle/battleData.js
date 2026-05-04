d5e49545b27f411456972134b921f6ba11d35e41
akisuteno-game <akisuteno5160@gmail.com>
2026-05-04 21:37:33 +0900

    Update battleData.js

--- battle/battleData.js
+++ battle/battleData.js
@@ -3,22 +3,62 @@ const enemies = [
   {
     id: 1,
     name: "Slime",
+
     hp: 100,
-    attack: 10
+    attack: 8,
+    speed: 900,
+
+    image: "./img/slime.png",
+
+    drops: [
+      "slimeGel"
+    ]
   },
 
   {
     id: 2,
     name: "Goblin",
-    hp: 200,
-    attack: 20
+
+    hp: 180,
+    attack: 15,
+    speed: 700,
+
+    image: "./img/goblin.png",
+
+    drops: [
+      "goblinEar"
+    ]
   },
 
   {
     id: 3,
+    name: "Orc",
+
+    hp: 300,
+    attack: 25,
+    speed: 550,
+
+    image: "./img/orc.png",
+
+    drops: [
+      "orcSkin"
+    ]
+  },
+
+  {
+    id: 4,
     name: "Dragon",
-    hp: 500,
-    attack: 50
+
+    hp: 1000,
+    attack: 50,
+    speed: 350,
+
+    image: "./img/dragon.png",
+
+    drops: [
+      "dragonScale",
+      "dragonHeart"
+    ]
   }
 
 ];
