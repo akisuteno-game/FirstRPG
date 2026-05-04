3180ab80d7d1d1c208f3ee7d6e5956c5d784016d
akisuteno-game <akisuteno5160@gmail.com>
2026-05-04 13:14:11 +0900

    Update battleAction.js

--- battle/battleAction.js
+++ battle/battleAction.js
@@ -23,6 +23,32 @@ function attackEnemy(){
 
 
 
+  playerGauge = 0;
+
+
+
+
+  const gauge =
+
+    document.getElementById(
+      "playerGauge"
+    );
+
+
+
+
+  if(
+    gauge
+  ){
+
+    gauge.style.width =
+      "0%";
+
+  }
+
+
+
+
   currentEnemy.hp -=
     player.atk;
 
@@ -162,161 +188,6 @@ function attackEnemy(){
 
 
   startPlayerGauge();
-
-
-}
-
-
-
-
-function enemyAttack(){
-
-
-  player.hp -=
-    currentEnemy.atk;
-
-
-
-
-  if(
-    player.hp < 0
-  ){
-
-    player.hp = 0;
-
-  }
-
-
-
-
-  savePlayer();
-
-
-
-
-  renderPlayer();
-
-
-
-
-  if(
-    player.hp <= 0
-  ){
-
-    setTimeout(
-
-      function(){
-
-        location.href =
-          "index.html";
-
-      },
-
-      500
-
-    );
-
-  }
-
-
-}
-
-
-
-
-function updateEnemyUI(){
-
-
-  const hpText =
-
-    document.getElementById(
-      "enemyHpText"
-    );
-
-
-
-
-  const hpBar =
-
-    document.getElementById(
-      "enemyHpFill"
-    );
-
-
-
-
-  if(
-    !hpText
-    ||
-    !hpBar
-  ){
-
-    return;
-
-  }
-
-
-
-
-  hpText.innerHTML =
-    currentEnemy.hp;
-
-
-
-
-  const maxHp =
-
-    enemies[
-      currentEnemy.id
-    ].hp;
-
-
-
-
-  const percent =
-
-    (
-      currentEnemy.hp
-      /
-      maxHp
-    ) * 100;
-
-
-
-
-  hpBar.style.width =
-
-    percent + "%";
-
-
-
-
-  if(
-    percent > 70
-  ){
-
-    hpBar.style.background =
-      "lime";
-
-  }
-
-
-  else if(
-    percent > 30
-  ){
-
-    hpBar.style.background =
-      "yellow";
-
-  }
-
-
-  else{
-
-    hpBar.style.background =
-      "red";
-
-  }
 
 
 }
