9640d577c9f921f852458b7b07a4dad98c44fd60
akisuteno-game <akisuteno5160@gmail.com>
2026-05-04 09:54:10 +0900

    Create gaugeRender.js

---
+++ battle/gaugeRender.js
@@ -0,0 +1,32 @@
+function renderGauges(){
+
+
+  return `
+
+    自分ゲージ
+
+    <div>
+
+      <div
+        id="playerGauge"
+      ></div>
+
+    </div>
+
+
+
+
+    敵ゲージ
+
+    <div>
+
+      <div
+        id="enemyGauge"
+      ></div>
+
+    </div>
+
+  `;
+
+
+}
