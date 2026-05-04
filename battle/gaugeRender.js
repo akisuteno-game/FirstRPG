abe66ac345e3838efd4232e8d698f2134219ec70
akisuteno-game <akisuteno5160@gmail.com>
2026-05-04 10:14:14 +0900

    Rename renderGauge to renderGauges

--- battle/gaugeRender.js
+++ battle/gaugeRender.js
@@ -1,28 +1,45 @@
-function renderGauge(){
+function renderGauges(){
 
 
   return `
 
+
     <div class="gaugeBox">
 
+
       <div
+
         id="enemyGauge"
+
         class="gaugeFill"
+
       ></div>
 
+
     </div>
+
+
+
+
+    <br><br>
 
 
 
 
     <div class="gaugeBox">
 
+
       <div
+
         id="playerGauge"
+
         class="gaugeFill"
+
       ></div>
 
+
     </div>
+
 
   `;
 
