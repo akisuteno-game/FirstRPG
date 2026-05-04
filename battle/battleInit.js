00bb21d5f3db69bda54463d0562c629f0ecbd793
akisuteno-game <akisuteno5160@gmail.com>
2026-05-04 21:14:26 +0900

    Update battleInit.js

--- battle/battleInit.js
+++ battle/battleInit.js
@@ -1,77 +1,53 @@
-console.log("battleInit start");
-
 const enemyId =
   localStorage.getItem(
     "selectedEnemy"
   );
 
-console.log(
-  "enemyId =",
-  enemyId
-);
-
 if(
   !enemyId
+  ||
+  !enemies[enemyId]
 ){
-  alert(
-    "enemyIdなし"
-  );
-
   location.href =
-    "../index.html";
+    "index.html";
 }
 
-if(
-  !enemies[
-    enemyId
-  ]
-){
-  alert(
-    "enemyデータなし : " +
-    enemyId
-  );
+else{
 
-  location.href =
-    "../index.html";
+  currentEnemy =
+
+    JSON.parse(
+
+      JSON.stringify(
+
+        enemies[
+          enemyId
+        ]
+
+      )
+
+    );
+
+
+
+  currentEnemy.maxHp =
+
+    currentEnemy.hp;
+
+
+
+  renderBattle();
+
+
+
+  renderPlayer();
+
+
+
+  startPlayerGauge();
+
+
+
+  startEnemyGauge();
+
 }
-
-currentEnemy =
-  JSON.parse(
-    JSON.stringify(
-      enemies[
-        enemyId
-      ]
-    )
-  );
-
-currentEnemy.maxHp =
-  currentEnemy.hp;
-
-console.log(
-  "enemy loaded",
-  currentEnemy
-);
-
-renderBattle();
-
-console.log(
-  "renderBattle OK"
-);
-
-renderPlayer();
-
-console.log(
-  "renderPlayer OK"
-);
-
-startPlayerGauge();
-
-console.log(
-  "playerGauge OK"
-);
-
-startEnemyGauge();
-
-console.log(
-  "enemyGauge OK"
-);
