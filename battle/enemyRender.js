bae0b3d151119aea2aca7e850188ad4ef8c2a100
akisuteno-game <akisuteno5160@gmail.com>
2026-05-04 10:08:07 +0900

    Update enemyRender.js

--- battle/enemyRender.js
+++ battle/enemyRender.js
@@ -3,31 +3,85 @@ function renderEnemy(){
 
   return `
 
-    <h2>
-
-      ${currentEnemy.name}
-
-    </h2>
+    <div class="enemyBox">
 
 
+      <img
+        class="enemyImage"
+        src="${currentEnemy.img}"
+      >
 
 
-    <img
 
-      class="
-        enemyImg
-      "
 
-      src="${
-        currentEnemy.img
-      }"
+      <div class="battleInfo">
 
-      onerror="
-        this.style.display=
-        'none'
-      "
 
-    >
+        <div>
+
+          ${currentEnemy.name}
+
+        </div>
+
+
+
+
+        <div class="hpRow">
+
+          HP :
+          <span id="enemyHpText">
+
+            ${currentEnemy.hp}
+
+          </span>
+
+        </div>
+
+
+
+
+        <div>
+
+          攻撃速度 :
+          ${currentEnemy.speed}
+
+        </div>
+
+
+
+
+        <div class="gaugeBox">
+
+          <div
+
+            id="enemyGauge"
+
+            class="gaugeFill"
+
+          ></div>
+
+        </div>
+
+
+
+
+        <div class="gaugeBox">
+
+          <div
+
+            id="playerGauge"
+
+            class="gaugeFill"
+
+          ></div>
+
+        </div>
+
+
+      </div>
+
+
+    </div>
 
   `;
 
