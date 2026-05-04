c69786cb1c00a3f626bccc1379d31026f3e6795e
akisuteno-game <akisuteno5160@gmail.com>
2026-05-04 09:54:30 +0900

    Create buttonRender.js

---
+++ battle/buttonRender.js
@@ -0,0 +1,48 @@
+function renderButtons(){
+
+
+  return `
+
+    <button
+
+      id="
+        attackBtn
+      "
+
+      onclick="
+        attackEnemy()
+      "
+
+      disabled
+
+    >
+
+      攻撃
+
+    </button>
+
+
+
+
+    <br><br>
+
+
+
+
+    <button
+
+      onclick="
+        location.href=
+        'index.html'
+      "
+
+    >
+
+      戻る
+
+    </button>
+
+  `;
+
+
+}
