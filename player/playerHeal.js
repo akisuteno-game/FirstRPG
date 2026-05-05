7f9e59ef86ae5f364046a6890822f2ccf308fd75
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 10:39:01 +0900

    Update playerHeal.js

--- player/playerHeal.js
+++ player/playerHeal.js
@@ -5,103 +5,39 @@ let healLoop = null;
 
 function startAutoHeal(){
 
-
   if(
-
-    location.pathname.includes(
-      "battle"
-    )
-
+    location.pathname.includes("battle")
   ){
-
     return;
-
   }
 
+  clearInterval(healLoop);
 
+  healLoop = setInterval(
 
+    function(){
 
-  clearInterval(
-    healLoop
+      if(player.hp >= player.maxHp){
+        return;
+      }
+
+      const heal =
+        Math.ceil(player.maxHp * 0.1);
+
+      player.hp += heal;
+
+      if(player.hp > player.maxHp){
+        player.hp = player.maxHp;
+      }
+
+      savePlayer();
+      renderPlayer();
+
+    },
+
+    1000
+
   );
-
-
-
-
-  healLoop =
-
-    setInterval(
-
-
-      function(){
-
-
-        if(
-
-          player.hp
-          >=
-          player.maxHp
-
-        ){
-
-          return;
-
-        }
-
-
-
-
-        const heal =
-
-          Math.ceil(
-
-            player.maxHp
-            *
-            0.1
-
-          );
-
-
-
-
-        player.hp +=
-          heal;
-
-
-
-
-        if(
-
-          player.hp
-          >
-          player.maxHp
-
-        ){
-
-          player.hp =
-            player.maxHp;
-
-        }
-
-
-
-
-        savePlayer();
-
-
-
-
-        renderPlayer();
-
-
-      },
-
-
-      1000
-
-
-    );
-
 
 }
 
