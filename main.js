b1c62c904abc3eab4883b6a880629bf93d81138a
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 10:36:30 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,114 +1,25 @@
 function showTab(tabId){
 
-
   const pages =
+    document.querySelectorAll(".tabPage");
 
-    document.querySelectorAll(
-      ".tabPage"
-    );
+  pages.forEach(function(page){
+    page.classList.remove("activePage");
+  });
 
+  const tab = document.getElementById(tabId);
 
-
-
-  pages.forEach(
-
-    function(page){
-
-      page.classList.remove(
-        "activePage"
-      );
-
-    }
-
-  );
-
-
-
-
-  const tab =
-
-    document.getElementById(
-      tabId
-    );
-
-
-
-
-  if(
-    tab
-  ){
-
-      tab.classList.add(
-        "activePage"
-      );
-
+  if(tab){
+    tab.classList.add("activePage");
   }
 
-
 }
 
 
 
 
-
-if(
-  typeof renderEnemyTab
-  ===
-  "function"
-){
-
-  renderEnemyTab();
-
-}
-
-
-
-
-if(
-  typeof renderUpgradeTab
-  ===
-  "function"
-){
-
-  renderUpgradeTab();
-
-}
-
-
-
-
-if(
-  typeof renderMaterialTab
-  ===
-  "function"
-){
-
-  renderMaterialTab();
-
-}
-
-
-
-
-if(
-  typeof renderSettingTab
-  ===
-  "function"
-){
-
-  renderSettingTab();
-
-}
-
-
-
-
-if(
-  typeof renderPlayer
-  ===
-  "function"
-){
-
-  renderPlayer();
-
-}
+if(typeof renderEnemyTab   === "function"){ renderEnemyTab();   }
+if(typeof renderUpgradeTab === "function"){ renderUpgradeTab(); }
+if(typeof renderMaterialTab=== "function"){ renderMaterialTab();}
+if(typeof renderSettingTab === "function"){ renderSettingTab(); }
+if(typeof renderPlayer     === "function"){ renderPlayer();     }
