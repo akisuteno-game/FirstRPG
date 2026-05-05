677093188cd8a170e09071c90e821edbb2753cbb
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 10:39:51 +0900

    Update materialIcon.js

--- player/materialIcon.js
+++ player/materialIcon.js
@@ -1,35 +1,16 @@
 function getMaterialPath(name){
 
-
   const paths = {
-
-    "スライムゼリー":
-    "img/materials/slimeGel.png",
-
-
-
-
-    "キングゼリー":
-    "img/materials/kingGel.png",
-
-
-
-
-    "ゴブリンの牙":
-    "img/materials/goblinTooth.png",
-
-
-
-
-    "王族の牙":
-    "img/materials/kingTooth.png"
-
+    "スライムゼリー": "img/materials/slimeGel.png",
+    "キングゼリー":   "img/materials/kingGel.png",
+    "ゴブリンの牙":   "img/materials/goblinTooth.png",
+    "王族の牙":       "img/materials/kingTooth.png",
+    "オークの皮":     "img/materials/orcSkin.png",
+    "古代の皮":       "img/materials/ancientSkin.png",
+    "竜のウロコ":     "img/materials/dragonScale.png",
+    "紅竜の心臓":     "img/materials/dragonHeart.png"
   };
 
-
-
-
   return paths[name] || null;
-
 
 }
