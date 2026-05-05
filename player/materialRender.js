5c3c1e4bf404455d6bae3d7077165fafc73d508c
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 10:40:12 +0900

    Update materialRender.js

--- player/materialRender.js
+++ player/materialRender.js
@@ -1,145 +1,46 @@
 function createMaterialHTML(){
 
-
-  if(
-    !player.materials
-  ){
-
+  if(!player.materials){
     return "なし";
-
   }
-
-
-
 
   const order = [
-
     "スライムゼリー",
-
     "キングゼリー",
-
     "ゴブリンの牙",
-
     "王族の牙",
-
     "オークの皮",
-
     "古代の皮",
-
     "竜のウロコ",
-
     "紅竜の心臓"
-
   ];
 
+  let html = "";
 
+  order.forEach(function(name){
 
+    const amount = player.materials[name];
 
-  let html =
-    "";
-
-
-
-
-  order.forEach(
-
-    function(name){
-
-
-      const amount =
-
-        player.materials[
-          name
-        ];
-
-
-
-
-      if(
-        !amount
-      ){
-
-        return;
-
-      }
-
-
-
-
-      const path =
-
-        getMaterialPath(
-          name
-        );
-
-
-
-
-      let icon =
-        "？";
-
-
-
-
-      if(
-        path
-      ){
-
-        icon = `
-
-          <img
-
-            src="${path}"
-
-            width="22"
-
-            onerror="
-              this.outerHTML='？'
-            "
-
-          >
-
-        `;
-
-      }
-
-
-
-
-      html += `
-
-        <div>
-
-          ${icon}
-
-          ×
-
-          ${amount}
-
-        </div>
-
-      `;
-
-
+    if(!amount){
+      return;
     }
 
-  );
+    const path = getMaterialPath(name);
 
+    let icon = "？";
 
+    if(path){
+      icon = `<img src="${path}" width="22" onerror="this.outerHTML='？'">`;
+    }
 
+    html += `<div>${icon} × ${amount}</div>`;
 
-  if(
-    html === ""
-  ){
+  });
 
+  if(html === ""){
     return "なし";
-
   }
 
-
-
-
   return html;
-
 
 }
