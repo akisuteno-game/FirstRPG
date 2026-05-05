6af65def9a34147c00943599f295313990a07193
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 10:41:44 +0900

    Update battleGauge.js

--- battle/battleGauge.js
+++ battle/battleGauge.js
@@ -1,146 +1,47 @@
 let playerGauge = 0;
-
-let enemyGauge = 0;
-
-let playerLoop = null;
-
-let enemyLoop = null;
+let enemyGauge  = 0;
+let playerLoop  = null;
+let enemyLoop   = null;
 
 
 
 
 function startPlayerGauge(){
 
-
-  clearInterval(
-    playerLoop
-  );
-
-
-
+  clearInterval(playerLoop);
 
   playerGauge = 0;
 
+  const bar    = document.getElementById("playerGauge");
+  const button = document.getElementById("attackBtn");
 
+  if(bar)   { bar.style.width    = "0%";  }
+  if(button){ button.disabled    = true;  }
 
+  const speed = player.attackSpeed;
 
-  const bar =
+  playerLoop = setInterval(
 
-    document.getElementById(
-      "playerGauge"
-    );
+    function(){
 
+      playerGauge += (16 / speed) * 100;
 
+      if(playerGauge >= 100){
 
+        playerGauge = 100;
+        clearInterval(playerLoop);
 
-  const button =
+        if(button){ button.disabled = false; }
 
-    document.getElementById(
-      "attackBtn"
-    );
+      }
 
+      if(bar){ bar.style.width = playerGauge + "%"; }
 
+    },
 
+    16
 
-  if(
-    bar
-  ){
-
-    bar.style.width =
-      "0%";
-
-  }
-
-
-
-
-  if(
-    button
-  ){
-
-    button.disabled =
-      true;
-
-  }
-
-
-
-
-  const speed =
-
-    player.attackSpeed;
-
-
-
-
-  playerLoop =
-
-    setInterval(
-
-
-      function(){
-
-
-        playerGauge +=
-
-          (
-            16
-            /
-            speed
-          ) * 100;
-
-
-
-
-        if(
-          playerGauge >= 100
-        ){
-
-          playerGauge = 100;
-
-
-
-
-          clearInterval(
-            playerLoop
-          );
-
-
-
-
-          if(
-            button
-          ){
-
-            button.disabled =
-              false;
-
-          }
-
-        }
-
-
-
-
-        if(
-          bar
-        ){
-
-          bar.style.width =
-
-            playerGauge + "%";
-
-        }
-
-
-      },
-
-
-      16
-
-
-    );
-
+  );
 
 }
 
@@ -149,100 +50,35 @@ function startPlayerGauge(){
 
 function startEnemyGauge(){
 
-
-  clearInterval(
-    enemyLoop
-  );
-
-
-
+  clearInterval(enemyLoop);
 
   enemyGauge = 0;
 
+  const bar   = document.getElementById("enemyGauge");
 
+  if(bar){ bar.style.width = "0%"; }
 
+  const speed = currentEnemy.speed;
 
-  const bar =
+  enemyLoop = setInterval(
 
-    document.getElementById(
-      "enemyGauge"
-    );
+    function(){
 
+      enemyGauge += (16 / speed) * 100;
 
+      if(enemyGauge >= 100){
 
+        enemyGauge = 0;
+        enemyAttack();
 
-  if(
-    bar
-  ){
+      }
 
-    bar.style.width =
-      "0%";
+      if(bar){ bar.style.width = enemyGauge + "%"; }
 
-  }
+    },
 
+    16
 
-
-
-  const speed =
-
-    currentEnemy.speed;
-
-
-
-
-  enemyLoop =
-
-    setInterval(
-
-
-      function(){
-
-
-        enemyGauge +=
-
-          (
-            16
-            /
-            speed
-          ) * 100;
-
-
-
-
-        if(
-          enemyGauge >= 100
-        ){
-
-          enemyGauge = 0;
-
-
-
-
-          enemyAttack();
-
-        }
-
-
-
-
-        if(
-          bar
-        ){
-
-          bar.style.width =
-
-            enemyGauge + "%";
-
-        }
-
-
-      },
-
-
-      16
-
-
-    );
-
+  );
 
 }
