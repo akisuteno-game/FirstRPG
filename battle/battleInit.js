e4682eb6873266699cffbda2740093f444758476
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 09:44:04 +0900

    Update battleInit.js

--- battle/battleInit.js
+++ battle/battleInit.js
@@ -1,199 +1,56 @@
-let currentEnemy = null;
+document.addEventListener(
 
+  "DOMContentLoaded",
 
+  function(){
 
+    const area =
 
-function initBattle(){
-
-  try{
-
-    console.log(
-      "INIT START"
-    );
-
-
-
-
-    const enemyId =
-
-      localStorage.getItem(
-        "selectedEnemy"
-      );
-
-
-
-
-    console.log(
-      "enemyId = ",
-      enemyId
-    );
-
-
-
-
-    if(
-      enemyId === null
-    ){
-      return;
-    }
-
-
-
-
-    currentEnemy =
-
-      enemies.find(
-
-        function(enemy){
-
-          return (
-
-            Number(
-              enemy.id
-            )
-
-            ===
-
-            Number(
-              enemyId
-            )
-
-          );
-
-        }
-
-      );
-
-
-
-
-    console.log(
-      "FOUND = ",
-      currentEnemy
-    );
-
-
-
-
-    if(
-      !currentEnemy
-    ){
-
-      document
-        .getElementById(
-          "battleArea"
-        )
-        .innerHTML =
-
-        "<h1 style='color:white'>enemy not found</h1>";
-
-      return;
-
-    }
-
-
-
-
-    currentEnemy.maxHp =
-
-      currentEnemy.hp;
-
-
-
-
-    renderBattle();
-
-    console.log(
-      "renderBattle OK"
-    );
-
-
-
-
-    if(
-      typeof renderPlayer
-      ===
-      "function"
-    ){
-
-      renderPlayer();
-
-      console.log(
-        "renderPlayer OK"
-      );
-
-    }
-
-
-
-
-    if(
-      typeof startPlayerGauge
-      ===
-      "function"
-    ){
-
-      startPlayerGauge();
-
-      console.log(
-        "startPlayerGauge OK"
-      );
-
-    }
-
-
-
-
-    if(
-      typeof startEnemyGauge
-      ===
-      "function"
-    ){
-
-      startEnemyGauge();
-
-      console.log(
-        "startEnemyGauge OK"
-      );
-
-    }
-
-  }
-
-  catch(error){
-
-    document
-      .getElementById(
+      document.getElementById(
         "battleArea"
-      )
-      .innerHTML =
+      );
 
-      `
-      <h1 style="color:red">
 
-        ERROR
+
+
+    if(
+      !area
+    ){
+
+      alert(
+        "battleArea none"
+      );
+
+      return;
+
+    }
+
+
+
+
+    area.innerHTML = `
+
+      <h1
+        style="
+          color:white;
+          text-align:center;
+          margin-top:200px;
+        "
+      >
+
+        BATTLE INIT OK
 
       </h1>
 
-      <pre style="color:white">
-
-        ${error}
-
-      </pre>
-      `;
+    `;
 
 
 
 
-    console.error(
-      error
+    console.log(
+      "BATTLE INIT OK"
     );
 
   }
 
-}
-
-
-
-
-initBattle();
+);
