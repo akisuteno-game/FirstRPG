790ce44ee8b061c29203ab3e8bc05b1e12992007
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 10:42:37 +0900

    Update gaugeRender.js

--- battle/gaugeRender.js
+++ battle/gaugeRender.js
@@ -1,50 +1,21 @@
 function renderGauges(){
 
-
   return `
 
     <div>
-
       自分ゲージ
-
       <div class="gaugeBox">
-
-        <div
-          id="playerGauge"
-          class="gaugeFill"
-          style="
-            width:0%;
-            background:lime;
-          "
-        ></div>
-
+        <div id="playerGauge" class="gaugeFill" style="width:0%;background:lime;"></div>
       </div>
-
     </div>
 
-
-
-
     <div>
-
       敵ゲージ
-
       <div class="gaugeBox">
-
-        <div
-          id="enemyGauge"
-          class="gaugeFill"
-          style="
-            width:0%;
-            background:red;
-          "
-        ></div>
-
+        <div id="enemyGauge" class="gaugeFill" style="width:0%;background:red;"></div>
       </div>
-
     </div>
 
   `;
-
 
 }
