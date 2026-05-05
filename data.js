575c4b260cf19f15337adf6d55f919746212f046
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 10:35:37 +0900

    Update data.js

--- data.js
+++ data.js
@@ -1,100 +1,59 @@
 const enemies = [
 
   {
-
-    id:0,
-
-    name:"スライム",
-
-    hp:10,
-
-    atk:2,
-
-    speed:2500,
-
-    drop:5,
-
-    material:"スライムゼリー",
-
-    rareMaterial:"キングゼリー",
-
-    img:"img/monsters/slime.png"
-
+    id: 0,
+    name: "スライム",
+    hp: 10,
+    atk: 2,
+    speed: 2500,
+    drop: 5,
+    exp: 3,
+    material: "スライムゼリー",
+    rareMaterial: "キングゼリー",
+    rareChance: 0.1,
+    img: "img/monsters/slime.png"
   },
 
-
-
-
   {
-
-    id:1,
-
-    name:"ゴブリン",
-
-    hp:20,
-
-    atk:3,
-
-    speed:2200,
-
-    drop:10,
-
-    material:"ゴブリンの牙",
-
-    rareMaterial:"王族の牙",
-
-    img:"img/monsters/goblin.png"
-
+    id: 1,
+    name: "ゴブリン",
+    hp: 20,
+    atk: 3,
+    speed: 2200,
+    drop: 10,
+    exp: 6,
+    material: "ゴブリンの牙",
+    rareMaterial: "王族の牙",
+    rareChance: 0.08,
+    img: "img/monsters/goblin.png"
   },
 
-
-
-
   {
-
-    id:2,
-
-    name:"オーク",
-
-    hp:30,
-
-    atk:4,
-
-    speed:2000,
-
-    drop:15,
-
-    material:"オークの皮",
-
-    rareMaterial:"古代の皮",
-
-    img:"img/monsters/orc.png"
-
+    id: 2,
+    name: "オーク",
+    hp: 30,
+    atk: 4,
+    speed: 2000,
+    drop: 15,
+    exp: 10,
+    material: "オークの皮",
+    rareMaterial: "古代の皮",
+    rareChance: 0.07,
+    img: "img/monsters/orc.png"
   },
 
-
-
-
   {
-
-    id:3,
-
-    name:"ドラゴン",
-
-    hp:50,
-
-    atk:6,
-
-    speed:1500,
-
-    drop:30,
-
-    material:"竜のウロコ",
-
-    rareMaterial:"紅竜の心臓",
-
-    img:"img/monsters/dragon.png"
-
+    id: 3,
+    name: "ドラゴン",
+    hp: 50,
+    atk: 6,
+    speed: 1500,
+    drop: 30,
+    exp: 20,
+    material: "竜のウロコ",
+    rareMaterial: "紅竜の心臓",
+    rareChance: 0.05,
+    img: "img/monsters/dragon.png"
   }
 
 ];
