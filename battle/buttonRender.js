d7f9b2e33b6b8651453151a67405b0ff720aaa7a
akisuteno-game <akisuteno5160@gmail.com>
2026-05-05 11:02:06 +0900

    Update buttonRender.js

--- battle/buttonRender.js
+++ battle/buttonRender.js
@@ -21,7 +21,7 @@ function renderButtons(){
 
       <button
         class="battleBtn"
-        onclick="location.href='index.html'"
+        onclick="location.href='../index.html'"
       >
         戻る
       </button>
