8736d767ccc32f0ac5ac3ac1065e8ab332574e10
akisuteno-game <akisuteno5160@gmail.com>
2026-05-06 08:35:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,19 @@
 function showTab(tabId){
 
   const pages = document.querySelectorAll(".tabPage");
-
   pages.forEach(function(page){
     page.classList.remove("activePage");
   });
 
   const tab = document.getElementById(tabId);
-
   if(tab){ tab.classList.add("activePage"); }
 
 }
 
-
-
-
 if(typeof renderEnemyTab    === "function"){ renderEnemyTab();    }
 if(typeof renderUpgradeTab  === "function"){ renderUpgradeTab();  }
 if(typeof renderSkillTab    === "function"){ renderSkillTab();    }
+if(typeof renderCraftTab    === "function"){ renderCraftTab();    }
 if(typeof renderMaterialTab === "function"){ renderMaterialTab(); }
 if(typeof renderRebirthTab  === "function"){ renderRebirthTab();  }
 if(typeof renderSettingTab  === "function"){ renderSettingTab();  }
